a6bc72c24815daed97c60971b613773e8d13d18b
Mykola Haliuk <mykola.haliuk@plus4u.net>
2021-03-20 16:01:39 +0200

    Updates

---
+++ precache-manifest.1b4b8c0d7638d16fec613be10fc4a462.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a520f44babbcaf37c798e55c5a1113a",
+    "url": "/weater-app/index.html"
+  },
+  {
+    "revision": "6f8336459dff8889d16a",
+    "url": "/weater-app/static/css/main.a651bbec.chunk.css"
+  },
+  {
+    "revision": "2206c9ae82ea43a5e6cb",
+    "url": "/weater-app/static/js/2.00483704.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weater-app/static/js/2.00483704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8336459dff8889d16a",
+    "url": "/weater-app/static/js/main.8205eb93.chunk.js"
+  },
+  {
+    "revision": "2e98063df85bc264ac9a",
+    "url": "/weater-app/static/js/runtime-main.e61412ed.js"
+  },
+  {
+    "revision": "67de48022d4bb661ff881cce6d56a8a5",
+    "url": "/weater-app/static/media/avatar.67de4802.jpg"
+  },
+  {
+    "revision": "51e3480b12327eef171cfaecb8fc821c",
+    "url": "/weater-app/static/media/burger_icon.51e3480b.svg"
+  },
+  {
+    "revision": "ab285ce5323aaf42598df6341c8d1d8a",
+    "url": "/weater-app/static/media/city-wide.ab285ce5.jpg"
+  },
+  {
+    "revision": "c1354932e79319c9d3dcb21acc9c95ef",
+    "url": "/weater-app/static/media/exit.c1354932.svg"
+  },
+  {
+    "revision": "3a79b6d29dcf3a9f48368989548eeb16",
+    "url": "/weater-app/static/media/mountains.3a79b6d2.jpg"
+  }
+]);
